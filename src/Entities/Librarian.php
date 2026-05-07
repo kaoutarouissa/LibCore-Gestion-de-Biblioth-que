2e549fa24388bee398d1815709a3d1bf595838c1
kaoutar <kaoutar@gmail.com>
2026-05-07 12:40:47 +0100

    correction des imports de classes et activation de l'exécution des requêtes SQL dans LibraryService

--- src/Entities/Librarian.php
+++ src/Entities/Librarian.php
@@ -1,29 +1,28 @@
 <?php
 require_once 'User.php';
 require_once 'Book.php';
-require-once .Services/Library.php;
-echo "jhugugu";
+// require_once '../Services/Library.php';
 class Librarian extends User{
     protected $library;
-    public function __construct(string $name, string $email, string $type){
-        // return "Le membre " . $this->name . " de type " . $type->getType();
+    public function __construct(string $name, string $email, string $type, $library){
         parent::__construct($name, $email,$type);
         $this->library=$library;
         
         }
         
         public function AjouterLivre($titre,$auteur,$ISBN): string {
-            return "livre ajouté sous le nom : ".$this->library->addLivre($titre,$auteur,$ISBN) ;
+            $LivreAjouté=$this->library->addLivre($titre,$auteur,$ISBN);
+            return "livre ajouté sous le nom : ".$LivreAjouté;
     }
     public function AjouterCompt($nom,$email):string{
-    return "compt ajouté sous le nom : ".$this->addCompt() ;
+    return "compt ajouté  : ".$this->library->addCompt($nom,$email) ;
     }
     public function afficherLivre($titre):string{
-        return "livre affiché sous le nom : ".$this->getLivre() ;
+        return "livre affiché sous le nom : ".$this->getLivre($titre) ;
 
     }
     public function RetirerLivre($titre):string{
-return "livre retiré sous le nom : ".$this->removeLivre() ;
+return "livre retiré  : ".$this->library->removeLivre($titre) ;
     }
 }
 ?>
